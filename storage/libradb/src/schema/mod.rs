a6a4289e950eb3a92627043856ed6bd56e5531e8
Yimin Chang <60542264+yimin-chang@users.noreply.github.com>
2020-10-27 11:54:36 +0800

    Update mod.rs
    To add the Column Family Name of ledger_info to the schema.

--- storage/libradb/src/schema/mod.rs
+++ storage/libradb/src/schema/mod.rs
@@ -28,6 +28,7 @@ pub(super) const EVENT_BY_KEY_CF_NAME: ColumnFamilyName = "event_by_key";
 pub(super) const EVENT_CF_NAME: ColumnFamilyName = "event";
 pub(super) const JELLYFISH_MERKLE_NODE_CF_NAME: ColumnFamilyName = "jellyfish_merkle_node";
 pub(super) const LEDGER_COUNTERS_CF_NAME: ColumnFamilyName = "ledger_counters";
+pub(super) const LEDGER_INFO_CF_NAME: ColumnFamilyName = "ledger_info";
 pub(super) const STALE_NODE_INDEX_CF_NAME: ColumnFamilyName = "stale_node_index";
 pub(super) const TRANSACTION_CF_NAME: ColumnFamilyName = "transaction";
 pub(super) const TRANSACTION_ACCUMULATOR_CF_NAME: ColumnFamilyName = "transaction_accumulator";
